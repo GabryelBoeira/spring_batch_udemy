6f59a08ac3a001f7e6c50436866597739726fb4d
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-08-31 23:01:33 -0300

    Refactor Tasklet to use lambda expression
    Replaced the anonymous Tasklet implementation with a lambda expression for improved readability and conciseness. This simplifies the code while retaining the same functionality.

--- batch_old/PrimeiroJobSpringBatch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroJobSpringBatchConfig.java
+++ batch_old/PrimeiroJobSpringBatch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroJobSpringBatchConfig.java
@@ -2,12 +2,9 @@ package com.udemy.primeiroprojetospringbatch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -37,14 +34,11 @@ public class PrimeiroJobSpringBatchConfig {
 	public Step imprimeOlaStep() {
 		return stepBuilderFactory
 				.get("imprimeOlaStep")
-				.tasklet(new Tasklet() {
-
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Olá, mundo!");
-						return RepeatStatus.FINISHED;
-					}
-				})
+				.tasklet((contribution, chunkContext) -> {
+                    System.out.println("Olá, mundo!");
+                    return RepeatStatus.FINISHED;
+                })
 				.build();
 	}
+
 }
